9149ea1d1fe0bc329fffca529da65cdb6ae7e4fe
Seungchan Park <ark10806@gmail.com>
2021-07-05 20:14:21 +0900

    ds

--- https_comm/db2.js
+++ https_comm/db2.js
@@ -4,7 +4,8 @@ var con = mysql.createConnection({
 	host:	'165.246.44.142',
 	port:	3306,
 	user:	'plc',
-	password: '1234'
+	password: '1234',
+	database: 'tmp_plc'
 });
 
 con.connect(function(err){
@@ -16,8 +17,9 @@ var sql = "SELECT * FROM cpscps";
 con.query(sql, function(err, rows, fields){
 	if(err){
 		console.log("query failed");
+		console.log(err);
 	} else{
-		console.log(rows.name);
+		console.log(rows);
 	}
 });
 
