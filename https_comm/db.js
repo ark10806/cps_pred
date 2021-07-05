da09c876618cb75c7b4e14d64d2d07024c29f684
ark10806 <ark10806@gmail.com>
2021-07-05 19:06:55 +0900

    to pc

--- https_comm/db.js
+++ https_comm/db.js
@@ -1,21 +1,25 @@
-var mysql = require('mysql');
-var conn = mysql.createConnection({
+var maria = require('mariadb');
+const pool = maria.createPool({
 
 	//host:		'165.246.44.142:3306',
 	host:		'165.246.44.142',
 	port:		3306,
-	user:		'root',
-	password:	'13130132',
-	database:	'cps'
+	user:		'plc',
+	password:	'1234',
+	//database:	'tmp_plc'
 });
 
-conn.connect();
+async function getUserList() {
+  let conn, rows;
+  try {
+    conn = await pool.getConnection();
+    conn.query('USE mysql'); // 사용할 DB 명시
+    rows = await conn.query('SELECT * FROM users'); // 쿼리 실행
+  }
+  catch (err) { throw err; }
+  finally {
+    if (conn) conn.end();
+    return rows;
+  }
+}
 
-conn.query('SECECT * FROM cps', function(error, results, fields){
-	if(error){
-		console.log(error);
-	}
-	console.log(results);
-});
-
-conn.end();
